3f09fc743a227bb94bbee67fbefc6ecc0e5254b1
Jeffrey <slassarg20@gmail.com>
2022-04-14 11:01:26 -0400

    ImageListComponent Alignment

--- src/Components/ImageListComponet.js
+++ src/Components/ImageListComponet.js
@@ -9,46 +9,41 @@ export const ImageListComponent = ({data}) => {
         setNumberOfElements(numberOfElements + numberOfElements)
     }
     const slice = data.slice(0,numberOfElements)
-    
 
     return(
         <div>
             <Container>
-               
-                    <ImageList variant="masonry" cols={4} gap={4}>
-                        {slice.map((item) => (
-    
-                            <ImageListItem key={item.id} height ="500">
-                                    <Card  >
-                                        <CardActionArea >
-                                            <CardMedia
-                                            component="img"
-                                            height="100"
-                                            image={item.url}
-                                            alt="green iguana"
-                                            />
-                                            <CardContent>
-                                                <Typography gutterBottom variant="h6" component="div">
-                                                    {item.camera + "          "}
-                                                    {item.date}
-                                                </Typography>
-                                                <Button size="small" color="primary" >
-                                                Delete
-                                                </Button>
-                                            </CardContent>
-                                        </CardActionArea>
-                                    </Card>
-                            </ImageListItem>
-                        ))}
-                        
-                    </ImageList>
-                    <hr></hr>
-                    <center>
-                        <Button variant="contained" disableElevation onClick ={() => loadMore()}>
-                            Load More...
-                        </Button>
-                    </center>
-             
+                <ImageList variant="masonry" cols={4} gap={4}>
+                    {slice.map((item) => (    
+                        <ImageListItem key={item.id} height ="500">
+                            <Card  >
+                                <CardActionArea >
+                                    <CardMedia
+                                    component="img"
+                                    height="100"
+                                    image={item.url}
+                                    alt="green iguana"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h6" component="div">
+                                            {item.camera + " "}
+                                            {item.date}
+                                        </Typography>
+                                        <Button size="small" color="primary" >
+                                        Delete
+                                        </Button>
+                                    </CardContent>
+                                </CardActionArea>
+                            </Card>
+                        </ImageListItem>
+                    ))} 
+                </ImageList>
+                <hr></hr>
+                <center>
+                    <Button variant="contained" disableElevation onClick ={() => loadMore()}>
+                        Load More...
+                    </Button>
+                </center>
             </Container>
         </div>
     );
--- src/Components/NasaImagesApp.js
+++ src/Components/NasaImagesApp.js
@@ -10,7 +10,7 @@ const NasaImagesApp = () => {
            
         <Toolbar/>
           <GetImages/>
-
+        
         </div>
     )
 
